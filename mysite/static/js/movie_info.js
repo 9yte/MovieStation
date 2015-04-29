c53771b99111d07079b6dd0cb7a056a01a6883da
Hojjat Aghakhani <Hojjat.aghakhani72@gmail.com>
2015-04-29 21:43:22 +0430

    popover for movie information now available, date color changed in timeline.

---
+++ mysite/static/js/movie_info.js
@@ -0,0 +1,10 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
+
+/**
+ * Created by hojjat on 1/24/15 AD.
+ */
+$(document).ready(function () {
+    $('#movie-info').popover("");
+});
