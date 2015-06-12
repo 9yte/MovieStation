77463829e97fcf5c8013a04ae4ca0d088cf386ea
Hojjat Aghakhani <Hojjat.aghakhani72@gmail.com>
2015-06-12 18:16:28 +0430

    Movie Model fixed.

--- mysite/movie/models.py
+++ mysite/movie/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 # Create your models here.
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Movie(models.Model):
@@ -8,6 +9,8 @@ class Movie(models.Model):
     description = models.TextField(blank=True, null=False)
     IMDB_link = models.CharField(max_length=100)
     cover_photo = models.ImageField(upload_to='/movie_covers', null=True, blank=True)
-
+    rate = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(10)])
+    rate_numbers = models.FloatField(default=0)
+    
     def __str__(self):
         return "{}".format(self.name)
