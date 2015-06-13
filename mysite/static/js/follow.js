b64fc3d5fbb01ce450e1dad0dfac5ccf2ef7e1f3
pezzati <p.ezzati.ab@gmail.com>
2015-06-14 01:01:19 +0430

    followers/followering list added

--- mysite/static/js/follow.js
+++ mysite/static/js/follow.js
@@ -1,6 +1,6 @@
 $(".follow").click(function(){
     $target = $(this);
-    $.post("/user/" + $target.attr('data-follow'), {followed : this.id}, function(data){
+    $.post("/user/" + $target.attr('data-follow'), {followed : $target.attr('data-user')}, function(data){
         if(data.status == 'ok'){
             if($target.attr('data-follow') == 'follow'){
                 $target.attr('data-follow', 'unfollow');
